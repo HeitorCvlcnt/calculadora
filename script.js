9cd4951ee85a0f883224f63738c6b617587d5676
Heitor Natanael R. Cavalcanti <heitor_natta@hotmail.com>
2022-09-29 14:58:51 -0300

    Add files via upload
    Projeto completo!!

---
+++ script.js
@@ -0,0 +1,25 @@
+function insertValues(num) {
+   const input = document.getElementById('input-value').value
+   document.getElementById('input-value').value = input + num
+}
+
+function calculate() {
+   const input = document.getElementById('input-value').value
+   document.getElementById('input-value').value = eval(input)
+}
+
+function clearInput() {
+   const input = document.getElementById('input-value').value = ''
+}
+
+function invertValue() {
+   calculate()
+   const input = document.getElementById('input-value').value
+   document.getElementById('input-value').value = input * -1
+}
+//
+function percent() {
+   calculate()
+   const input = document.getElementById('input-value').value
+   document.getElementById('input-value').value = input / 100
+}
